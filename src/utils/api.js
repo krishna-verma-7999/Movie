eb937a48c9ebd9c5e95bc9cb39df59c199241e25
krishna <kv510341@gmail.com>
2023-10-12 21:16:13 +0530

    fixed project

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+// const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGFiMWZiYTljZDY2NjA4MzdiZGU0MTkyMjkyZmQwNyIsInN1YiI6IjY0NTY2OTJjOTFmMGVhMDBmZjExZjM3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ptK9Ce9XWRlm3FV9j-ESxwrC7dCdsBhuIS6FkPOXnmo';
 
 const headers = {
     Authorization: "bearer " + TMDB_TOKEN, // space after bearer is necessary
